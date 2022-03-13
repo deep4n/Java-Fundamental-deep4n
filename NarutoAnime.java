41cef217e37be9f668b89efdf4b305ce96026a73
deep4n <99375480+deep4n@users.noreply.github.com>
2022-03-14 01:14:49 +0700

    Add files via upload

---
+++ NarutoAnime.java
@@ -0,0 +1,17 @@
+package anime;
+
+/**
+ *
+ * @author mhddf
+ */
+public class NarutoAnime {
+
+    public static void main(String[] args) {
+        Sasuke s = new Sasuke();
+        s.printDojutsu();
+        
+        Itachi i = new Sasuke();
+        i.printKekkeiGenkai();
+    }
+}
+
