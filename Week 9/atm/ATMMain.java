6adc346b901b558678e4be6e95d24622329cd2cf
deep4n <99375480+deep4n@users.noreply.github.com>
2022-04-07 13:20:24 +0700

    Add files via upload

---
+++ Week 9/atm/ATMMain.java
@@ -0,0 +1,18 @@
+package atm;
+
+/**
+ *
+ * @author mhddf
+ */
+
+public class ATMMain {
+    // main method creates and runs the ATM
+    public static void main(String[] args) {
+        ATM theATM = new ATM();    
+        theATM.run();
+    }
+} 
+
+
+
+ 
